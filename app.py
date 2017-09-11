fc1c898a16002443ee4b643488f7dda39f546b29
szaneer@gmail.com <siraj@pal-nat186-14-75.itap.purdue.edu>
2017-09-11 00:57:41 -0400

    Os added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #flask/bin/python
 from flask import Flask, request, jsonify, json
 import requests
+import os
 
 app = Flask(__name__)
 pokeCache = {}
@@ -57,5 +58,5 @@ def pokemon():
 
 
 if __name__ == '__main__':
-   # port = int(os.environ.get('PORT', 5000))
-    app.run() #host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
